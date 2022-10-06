daa9ca78802e52d274424db334d4856d34ac8831
ozkostay <ozkostay@mail.ru>
2022-10-06 23:07:12 +0500

    addClasses 061022

--- src/js/character.js
+++ src/js/character.js
@@ -1,23 +1,16 @@
-export default function Character(name, type) {
-  if (name.length < 2 || name.length > 10) {
-    throw new Error('Недопустимая длина имени персонажа');
-  }
+export default class Character {
+  constructor(name, type) {
+    if (name.length < 2 || name.length > 10) {
+      throw new Error('Недопустимая длина имени персонажа');
+    }
+    const types = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+    if (!types.find((item) => item === type)) {
+      throw new Error('Недопустый тип персонажа');
+    }
 
-  const types = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
-  if (!types.find((item) => item === type)) {
-    throw new Error('Недопустый тип персонажа');
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
   }
-
-  this.name = name;
-  this.type = type;
-  this.health = 100;
-  this.level = 1;
-  this.attackProtection = {
-    Bowman: '25/25',
-    Swordsman: '40/10',
-    Magician: '10/40',
-    Undead: '25/25',
-    Zombie: '40/10',
-    Daemon: '10/40',
-  };
 }
